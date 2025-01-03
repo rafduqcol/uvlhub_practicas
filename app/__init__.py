2f23807376bc068df0ee6394081a0432b8417475
“Rafael <rafduqcol@alum.us.es>
2025-01-03 12:09:21 +0100

    Prueba SSH

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ from core.managers.config_manager import ConfigManager
 from core.managers.error_handler_manager import ErrorHandlerManager
 from core.managers.logging_manager import LoggingManager
 
-# Load environment variables
+# Load environment variable
 load_dotenv()
 
 # Create the instances
